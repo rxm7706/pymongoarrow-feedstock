badd527f4b2b18c4ccfe5ce37e9a8fa6010db894
Steven Silvester <steven.silvester@ieee.org>
2022-10-31 10:40:07 -0500

    use standard port

--- recipe/run_the_tests.sh
+++ recipe/run_the_tests.sh
@@ -2,12 +2,11 @@
 set -ex
 export DB_PATH="$SRC_DIR/temp-mongo-db"
 export LOG_PATH="$SRC_DIR/mongolog"
-export DB_PORT=27272
 export PID_FILE_PATH="$SRC_DIR/mongopidfile"
 
 mkdir "$DB_PATH"
 
-mongod --dbpath="$DB_PATH" --fork --logpath="$LOG_PATH" --port="$DB_PORT" --pidfilepath="$PID_FILE_PATH"
+mongod --dbpath="$DB_PATH" --fork --logpath="$LOG_PATH" --pidfilepath="$PID_FILE_PATH"
 
 # Remove the local copy of the source files
 rm -rf pymongoarrow
