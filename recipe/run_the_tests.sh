8bb94d749979f8f033f4b3aaf6ad42fa73ec1d38
Steven Silvester <steven.silvester@ieee.org>
2023-07-10 09:46:16 -0500

    revert to using mongodb again

--- recipe/run_the_tests.sh
+++ recipe/run_the_tests.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -ex
+export DB_PATH="$SRC_DIR/temp-mongo-db"
+export LOG_PATH="$SRC_DIR/mongolog"
+export PID_FILE_PATH="$SRC_DIR/mongopidfile"
 
-podman run --name mongodb --detach mongo:6.0.2
+mkdir "$DB_PATH"
+
+mongod --dbpath="$DB_PATH" --fork --logpath="$LOG_PATH" --pidfilepath="$PID_FILE_PATH"
 
 # Remove the local copy of the source files
 rm -rf pymongoarrow
-pytest -v
+python -m unittest discover test -v
+
+# Terminate the forked process after the test suite exits
+kill `cat $PID_FILE_PATH`
