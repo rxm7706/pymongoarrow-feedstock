33276907770a464da41fb15a8f14ef0b0fe0cc84
Steven Silvester <steven.silvester@ieee.org>
2023-07-10 09:32:19 -0500

    try with podman

--- recipe/run_the_tests.sh
+++ recipe/run_the_tests.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -ex
-export DB_PATH="$SRC_DIR/temp-mongo-db"
-export LOG_PATH="$SRC_DIR/mongolog"
-export PID_FILE_PATH="$SRC_DIR/mongopidfile"
 
-mkdir "$DB_PATH"
-
-mongod --dbpath="$DB_PATH" --fork --logpath="$LOG_PATH" --pidfilepath="$PID_FILE_PATH"
+podman run --name mongodb --detach mongo:6.0.2
 
 # Remove the local copy of the source files
 rm -rf pymongoarrow
-python -m unittest discover test -v
-
-# Terminate the forked process after the test suite exits
-kill `cat $PID_FILE_PATH`
+pytest -v
